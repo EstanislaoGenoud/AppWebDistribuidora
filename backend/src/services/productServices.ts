090e4a725ef72ffecf56976b3fedbf571ca5a775
Estanislao Genoud <estangeno@gmail.com>
2025-06-19 19:02:34 -0300

    {Update: 8} Inicially developments of product controllers

--- backend/src/services/productServices.ts
+++ backend/src/services/productServices.ts
@@ -1,13 +1,35 @@
 import db from "../config/db";
+import { RowDataPacket } from "mysql2";
+
+interface Producto extends RowDataPacket {
+  idProduct: string;
+  legajo: string;
+  nombre: string;
+  marca: string;
+  categoria: string;
+  precioCompra: number;
+  idUnidad: number;
+  precioVenta: number;
+}
+export async function saveProductToDB(idProduct:string, legajo:string, nombre:string, marca:string, categoria:string, precioCompra:number, idUnidad:number, precioVenta:number) {
+  try {
+    const [result] = await db.query(
+      'INSERT INTO Producto (idProduct, legajo, nombre, marca, categoria, precioCompra, idUnidad, precioVenta) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+      [idProduct, legajo, nombre, marca, categoria, precioCompra, idUnidad, precioVenta]
+    );
+    return result;
+  } catch (error) {
+    console.error('Error saving product to database:', error);
+    throw error;
+  }
+}
 
 export async function getAllProductsFromDB() {
   try {
-    const [rows] = await db.query('SELECT * FROM Productos');
+    const [rows] = await db.query('SELECT * FROM Producto');
     return rows;
   } catch (error) {
     console.error('Error fetching all products from database:', error);
     throw error;
   }
 }
-
-// SEGUIR DESPUES DE CREAR LOS SERVICIOS Y CONTROLADORES DE PROVEEDORES
